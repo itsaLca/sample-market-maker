c185bb804eb0f97f7a3d608b07be70666da6935a
xyse <nicojuko@gmail.com>
2020-06-27 11:05:57 +0200

    Test1

--- market_maker/custom_strategy.py
+++ market_maker/custom_strategy.py
@@ -1,29 +1,16 @@
 import sys
 
-from market_maker.market_maker import OrderManager
+from market_maker.market_maker import ExchangeInterface
 
+print("Test")
 
-class CustomOrderManager(OrderManager):
-    """A sample order manager for implementing your own custom strategy"""
+class ParabolicSar:
+    def calculate_psar():
 
-    def place_orders(self) -> None:
-        # implement your custom strategy here
+class PsarOrderManager:
 
-        buy_orders = []
-        sell_orders = []
-
-        # populate buy and sell orders, e.g.
-        # buy_orders.append({'price': 999.0, 'orderQty': 100, 'side': "Buy"})
-        # sell_orders.append({'price': 1001.0, 'orderQty': 100, 'side': "Sell"})
-
-        self.converge_orders(buy_orders, sell_orders)
-
-
-def run() -> None:
-    order_manager = CustomOrderManager()
-
-    # Try/except just keeps ctrl-c from printing an ugly stacktrace
-    try:
-        order_manager.run_loop()
-    except (KeyboardInterrupt, SystemExit):
-        sys.exit()
+    def __init__(self):
+        self.exchange = ExchangeInterface(settings.DRY_RUN)
+        # Once exchange is created, register exit handler that will always cancel orders
+        # on any error.
+        print("Test")
